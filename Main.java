16d627214656e609f3b70cb15989787e96b5a20e
dcurtis14 <donald.curtis@patelhs.org>
2020-09-27 19:26:26 +0000

    i put line int myBirthday

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
-}
+    int myAge = 14;
+
+    System.out.println("i am"+ myAge + "years old");
+    int myBirthday = 06;
+
